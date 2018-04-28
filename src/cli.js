9238ee71ede922e95177d7496e34c089b60f2a1d
crimson-med <mederic.burlet@gmail.com>
2018-04-28 22:26:35 +0800

    console fix

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ const argv = require("yargs").argv;
 const proxyHandler = require("./handlers/proxyHandler");
 if (argv.tool === "proxy"){
   if (argv.proxyNumber === null){
-    Console.log("\nYou need to specify the number of proxies please try:\n\n" + colors.green("crimson-tools --tool help \n\n"));
+    console.log("\nYou need to specify the number of proxies please try:\n\n" + colors.green("crimson-tools --tool help \n\n"));
   }else{
     if (argv.country === null){
       proxyHandler.getProxies(argv.proxyNumber);
@@ -13,20 +13,20 @@ if (argv.tool === "proxy"){
     }
   }
 }else if (argv.tool === "help") {
-  Console.log("\n ----------------------------------------------");
-  Console.log(colors.red(" Crimson Tools"));
-  Console.log(" ----------------------------------------------");
-  Console.log(colors.green("\n  crimson-tools --tool type --optionalToolParameter optionalToolParameterValue"));
-  Console.log("\n ----------------------------------------------");
-  Console.log(colors.red(" Tool Types:"));
-  Console.log(" ----------------------------------------------");
-  Console.log("  --- "+colors.cyan("help")+" \n  --- "+colors.cyan("proxy"));
-  Console.log("  ------ "+colors.cyan("proxyNumber")+" = number of proxy to find eg: 5");
-  Console.log("  ------ "+colors.cyan("country")+" (optional) = two letter string eg: fr");
-  Console.log("\n ----------------------------------------------");
-  Console.log(colors.red(" Command examples:"));
-  Console.log(" ----------------------------------------------");
-  Console.log(colors.green("  crimson-tools --tool help"));
-  Console.log(colors.green("  crimson-tools --tool proxy --proxyNumber 5"));
-  Console.log(colors.green("  crimson-tools --tool proxy --proxyNumber 5 --country fr"));
+  console.log("\n ----------------------------------------------");
+  console.log(colors.red(" Crimson Tools"));
+  console.log(" ----------------------------------------------");
+  console.log(colors.green("\n  crimson-tools --tool type --optionalToolParameter optionalToolParameterValue"));
+  console.log("\n ----------------------------------------------");
+  console.log(colors.red(" Tool Types:"));
+  console.log(" ----------------------------------------------");
+  console.log("  --- "+colors.cyan("help")+" \n  --- "+colors.cyan("proxy"));
+  console.log("  ------ "+colors.cyan("proxyNumber")+" = number of proxy to find eg: 5");
+  console.log("  ------ "+colors.cyan("country")+" (optional) = two letter string eg: fr");
+  console.log("\n ----------------------------------------------");
+  console.log(colors.red(" Command examples:"));
+  console.log(" ----------------------------------------------");
+  console.log(colors.green("  crimson-tools --tool help"));
+  console.log(colors.green("  crimson-tools --tool proxy --proxyNumber 5"));
+  console.log(colors.green("  crimson-tools --tool proxy --proxyNumber 5 --country fr"));
 }
